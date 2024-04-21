114577c07b8c77f7e0e0f059a5afd5a13d6e6ad7
Daniel Larsson <eadaniel.larsson@gmail.com>
2024-04-22 00:02:51 +0200

    minor change to add optional chaining

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,13 @@ rl.on('line', chatBot.executeCommand);
 
 const exitHandler = (): void => {
   rl.close();
-  process.stdout.write('\nExit successful, have a great day! ');
+  process.stdout?.write('\nExit successful, have a great day! ');
   process.exit(0);
 };
 
 process.on('SIGINT', exitHandler);
 
 rl.on('close', (): void => {
-  process.stdout.write('\nExit successful, have a great day! ');
+  process.stdout?.write('\nExit successful, have a great day! ');
   process.exit(0);
 });
